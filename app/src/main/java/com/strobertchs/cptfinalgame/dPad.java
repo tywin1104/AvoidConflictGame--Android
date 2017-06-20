dae3e8bd786c160b6957eab170ea2ad0ec4691ce
Nathan L <Nathan L>
2017-06-19 22:09:13 -0400

    added dPad class

---
+++ app/src/main/java/com/strobertchs/cptfinalgame/dPad.java
@@ -0,0 +1,25 @@
+package com.strobertchs.cptfinalgame;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by Nathan on 2017-06-19.
+ */
+
+public class dPad extends Sprite {
+
+    public dPad(int dPad_width, int dPad_height, int positionX, int positionY)
+    {
+        super();
+
+        setPositionX(positionX);
+        setPositionY(positionY);
+
+        setWidth(dPad_width);
+        setHeight(dPad_height);
+    }
+
+    public void draw(Canvas source_canvas) {
+
+    }
+}
